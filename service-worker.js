81861a734860de53390442121c47ff147bb743c0
s9605293000-boop <s9605293000@gmail.com>
2025-10-07 23:50:46 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,24 @@
-const CACHE_NAME = "memory-duel-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/main.js",
-  "/assets/cards/",
-  "/assets/icons/icon-192x192.png",
-  "/assets/icons/icon-512x512.png"
-];
-
-// Установка service worker
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open("memory-duel-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/main.js",
+        "/manifest.webmanifest",
+        "/icons/icon-192.png",
+        "/icons/icon-512.png",
+        "/icons/maskable-icon.png"
+      ]);
     })
   );
 });
 
-// Работа в офлайн
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
-    })
-  );
-});
-
-// Обновление кэша
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
     })
   );
 });
