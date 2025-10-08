d189e02b29ee42e5aff02b97718f303dac4396b4
s9605293000-boop <s9605293000@gmail.com>
2025-10-07 23:34:52 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,43 @@
+const CACHE_NAME = "memory-duel-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/main.js",
+  "/assets/cards/",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png"
+];
 
-self.addEventListener('install', (e)=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open('md4-4').then(c=>c.addAll(['./','./index.html','./styles.css','./main.js','./lang.js','./config.js'])));
+// Установка service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-self.addEventListener('fetch',(e)=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+
+// Работа в офлайн
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Обновление кэша
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
