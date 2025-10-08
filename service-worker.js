f3b2640a1bf612d7e6a164074b48a120c654d8f8
s9605293000-boop <s9605293000@gmail.com>
2025-10-08 09:06:08 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,27 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("memory-duel-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/styles.css",
-        "/main.js",
-        "/manifest.webmanifest",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png",
-        "/icons/maskable-icon.png"
-      ]);
-    })
-  );
+const CACHE = 'memory-duel-v4-4';
+const ASSETS = [
+  '/', '/index.html', '/styles.css', '/main.js', '/lang.js',
+  '/manifest.webmanifest',
+  '/assets/icons/icon-192.png', '/assets/icons/icon-512.png',
+  '/assets/avatars/default.png',
+  '/assets/cards/question.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req))
   );
 });
